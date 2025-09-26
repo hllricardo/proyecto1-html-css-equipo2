f4f366917402da0a853ed5b5234cea7fb87c376c
sanpro23 <santosprogramacion23@hotmail.com>
2025-09-26 19:28:23 +0200

    script subido

---
+++ servicios2 js.js
@@ -0,0 +1,6 @@
+<script>
+    document.querySelectorAll('.div-show').forEach((el) => {
+        btn.addEventListener('click', () => {
+            e.preventDefault();
+            alert("Gracias por contactarnos, está habilitado el formulario de contacto)");
+</script>
