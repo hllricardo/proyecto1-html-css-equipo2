8e4c01d147283f7e8d06ac92e6da7919326dda7a
hllricardo <hernandezlloretricardo@.gmail.com>
2025-09-27 12:06:25 +0200

    barra lateral cuando la pantalla es pequeña añadido y funcionando, solo tiene un pequeño fallo que hay que solucionar

--- javascript.js
+++ javascript.js
@@ -1,6 +1,17 @@
+// Codigo para el boton de modo oscuro/claro
 const themeSwitch = document.getElementById("theme-switch");
 const body = document.getElementById("body");
 
 themeSwitch.addEventListener("change", () => {
   body.classList.toggle("dark-mode");
 });
+//---------------------------------------------------
+// Codigo para el menu lateral
+function openNav() {
+  document.getElementById("id_navigation_container").style.display = "block";
+}
+
+function closeNav() {
+  document.getElementById("id_navigation_container").style.display = "none";
+}
+//---------------------------------------------------
