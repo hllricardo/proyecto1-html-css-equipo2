4b226c433a3bbd354980f9cddb8bb47698e69ef0
hllricardo <hernandezlloretricardo@.gmail.com>
2025-09-28 12:02:23 +0200

    arreglado el fallo que dejaba el texto en blanco en equipo.html

--- javascript.js
+++ javascript.js
@@ -1,9 +1,14 @@
 // Codigo para el boton de modo oscuro/claro
 const themeSwitch = document.getElementById("theme-switch");
 const body = document.getElementById("body");
+const textEquipo = document.getElementsByClassName("divEquipo");
 
 themeSwitch.addEventListener("change", () => {
   body.classList.toggle("dark-mode");
+  for(let i=0; i<textEquipo.length; i++){
+    /*textEquipo[i].style.backgroundColor = "red";*/
+    textEquipo[i].classList.toggle("dark-mode-equipo");
+  }
 });
 //---------------------------------------------------
 // Codigo para el menu lateral
