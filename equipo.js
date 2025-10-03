3ed2619c069a2f61a85c7390881564f9a4b13cca
Julia Gonzalez <jgonzacu@gmail.com>
2025-10-03 16:39:00 +0200

    changes 3 oct

---
+++ equipo.js
@@ -0,0 +1,11 @@
+<script>
+    const observer = new IntersectionObserver((entries) = {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show');
+            }
+        });
+    });
+
+   document.querySelectorAll('.div-show').forEach((el) = observer.observe(el));
+</script>
