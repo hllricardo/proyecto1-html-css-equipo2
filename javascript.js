2fdc3c08f5f8e21d5be849cff855c13532b4b07d
JAVI <javierferfer20@gmail.com>
2025-10-07 18:42:13 +0200

    modificar javascript.js

--- javascript.js
+++ javascript.js
@@ -568,7 +568,7 @@ function showTerminal() {
 
 // estrellas reseñas //
 var logID = 'log',
-log = $('<div id="'+logID'"></div>');
+log = $('<div id = "'+logID+'"></div>');
 $('body').append(log);
 $('[type*="radio"]').change(function () {
   var me = $(this);
